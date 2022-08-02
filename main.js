31745fdf7b2e96487f1c648c7d0fafab104cafbb
jason <jduong89@gmail.com>
2022-08-02 16:39:22 -0700

    adds listener event for user

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const nameInput = document.querySelector("#name");
+  const newToDoForm = document.querySelector("#new-todo-form");
+
+  const username = localStorage.getItem("username") || "";
+
+  nameInput.value = username;
+
+  nameInput.addEventListener("change", (e) => {
+    localStorage.setItem("username", e.target.value);
+  });
+});
